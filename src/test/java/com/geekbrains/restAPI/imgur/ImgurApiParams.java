f5d4c6d72e633913e4c3ac980a88137b759b8909
ИВАН <jokovich@yandex.ru>
2021-11-08 21:03:30 +0300

    update

---
+++ src/test/java/com/geekbrains/restAPI/imgur/ImgurApiParams.java
@@ -0,0 +1,9 @@
+package com.geekbrains.restAPI.imgur;
+
+public class ImgurApiParams {
+
+    public static String TOKEN = "98881180f589c997049736f24f743c7c6b4ac19f";
+    public static String API_URL = "https://api.imgur.com";
+    public static String API_VERSION = "3";
+
+}
